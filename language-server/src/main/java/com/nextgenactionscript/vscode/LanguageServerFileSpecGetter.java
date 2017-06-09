29fea1e8ff0d3fb1e26fd0f5f83d7f68fe6fa805
Josh Tynjala <joshtynjala@gmail.com>
2017-06-08 20:26:41 -0700

    language-server: nope, those MXML workarounds are still needed for FlexJS 0.7.0 (which will still be supported until v0.5 of the extension)

--- language-server/src/main/java/com/nextgenactionscript/vscode/LanguageServerFileSpecGetter.java
+++ language-server/src/main/java/com/nextgenactionscript/vscode/LanguageServerFileSpecGetter.java
@@ -21,6 +21,7 @@ import java.util.Map;
 
 import org.apache.flex.compiler.common.IFileSpecificationGetter;
 import org.apache.flex.compiler.constants.IASKeywordConstants;
+import org.apache.flex.compiler.constants.IMXMLCoreConstants;
 import org.apache.flex.compiler.filespecs.FileSpecification;
 import org.apache.flex.compiler.filespecs.IFileSpecification;
 import org.apache.flex.compiler.internal.filespecs.StringFileSpecification;
@@ -36,7 +37,10 @@ import org.apache.flex.compiler.workspaces.IWorkspace;
  */
 public class LanguageServerFileSpecGetter implements IFileSpecificationGetter
 {
+    private static final String SCRIPT_START = "<fx:Script>";
+    private static final String SCRIPT_END = "</fx:Script>";
     private static final String PACKAGE_WITHOUT_BRACES = "package ";
+    private static final String FILE_EXTENSION_MXML = ".mxml";
     private static final String FILE_EXTENSION_AS = ".as";
 
     public LanguageServerFileSpecGetter(IWorkspace workspace, Map<Path, String> sourceByPath)
@@ -68,6 +72,13 @@ public class LanguageServerFileSpecGetter implements IFileSpecificationGetter
             {
                 code = fixPackageWithoutBraces(code);
             }
+            else if (filePath.endsWith(FILE_EXTENSION_MXML))
+            {
+                //these workaround are for FlexJS 0.7.0 and can be removed when
+                //that version is no longer supported
+                code = fixUnclosedXMLComment(code);
+                code = fixUnclosedScriptCDATA(code);
+            }
             return new StringFileSpecification(filePath, code);
         }
         return new FileSpecification(filePath);
@@ -88,5 +99,58 @@ public class LanguageServerFileSpecGetter implements IFileSpecificationGetter
             return code + "{}";
         }
         return code;
+    }
+
+    /**
+     * If an XML comment is not closed, the compiler will get into an infinite
+     * loop!
+     */
+    private String fixUnclosedXMLComment(String code)
+    {
+        int startComment = code.lastIndexOf(IMXMLCoreConstants.commentStart);
+        if (startComment == -1)
+        {
+            return code;
+        }
+        int endComment = code.indexOf(IMXMLCoreConstants.commentEnd, startComment);
+        if (endComment == -1)
+        {
+            return code + IMXMLCoreConstants.commentEnd;
+        }
+        return code;
+    }
+
+    /**
+     * If a <![CDATA[ inside <fx:Script></fx:Script> is not closed, the compiler
+     * will get into an infinite loop!
+     */
+    private String fixUnclosedScriptCDATA(String code)
+    {
+        int startIndex = 0;
+        do
+        {
+            int startScript = code.indexOf(SCRIPT_START, startIndex);
+            if (startScript == -1)
+            {
+                return code;
+            }
+            int endScript = code.indexOf(SCRIPT_END, startScript);
+            if (endScript == -1)
+            {
+                endScript = code.length();
+            }
+            int startCDATA = code.indexOf(IMXMLCoreConstants.cDataStart, startScript);
+            if (startCDATA != -1)
+            {
+                int endCDATA = code.lastIndexOf(IMXMLCoreConstants.cDataEnd, endScript);
+                if (endCDATA < startCDATA)
+                {
+                    code = code.substring(0, endScript) + IMXMLCoreConstants.cDataEnd + code.substring(endScript);
+                    endScript += IMXMLCoreConstants.cDataEnd.length();
+                }
+            }
+            startIndex = endScript;
+        }
+        while (true);
     }
 }
