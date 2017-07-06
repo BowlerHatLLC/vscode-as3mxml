b75523de7d2ba1b31779434a35fb09731159ea7f
Josh Tynjala <joshtynjala@gmail.com>
2017-07-06 15:46:24 -0700

    swf-debugger: error message says SWF instead of NextGen ActionScript now

--- swf-debugger/src/main/java/com/nextgenactionscript/vscode/SWFDebug.java
+++ swf-debugger/src/main/java/com/nextgenactionscript/vscode/SWFDebug.java
@@ -75,7 +75,7 @@ public class SWFDebug
             }
             catch (IOException e)
             {
-                System.err.println("NextGen ActionScript debug adapter failed to connect.");
+                System.err.println("SWF debug adapter failed to connect.");
                 System.err.println("Visit the following URL to file an issue, and please include this log: https://github.com/BowlerHatLLC/vscode-nextgenas/issues");
                 e.printStackTrace(System.err);
                 System.exit(SERVER_CONNECT_ERROR);
