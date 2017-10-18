316244152c16e98b8e5a4ca1377d44949e1278be
Josh Tynjala <joshtynjala@gmail.com>
2017-10-18 08:45:32 -0700

    vscode-extension: revamped the way that the language server is started and restarted to fix an issue on windows where deactivating the extension would fail with an error and leave the process running.
    As a bonus, it appears that the language server can restart more gracefully on Windows now, so there's no need to manually restart after changing the Java path or the nextgenas.sdk.editor.
    
    (references #143)

--- vscode-extension/src/main/ts/extension.ts
+++ vscode-extension/src/main/ts/extension.ts
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 import organizeImportsInTextEditor from "./commands/organizeImportsInTextEditor";
-import findPort from "./utils/findPort";
 import findJava from "./utils/findJava";
 import validateJava from "./utils/validateJava";
 import validateEditorSDK from "./utils/validateEditorSDK";
@@ -32,7 +31,8 @@ import * as net from "net";
 import * as path from "path";
 import * as vscode from "vscode";
 import {LanguageClient, LanguageClientOptions, SettingMonitor,
-	ServerOptions, StreamInfo, ErrorHandler, ErrorAction, CloseAction} from "vscode-languageclient";
+	ServerOptions, StreamInfo, ErrorHandler, ErrorAction,
+	CloseAction, Executable, ExecutableOptions} from "vscode-languageclient";
 import { Message } from "vscode-jsonrpc";
 
 const INVALID_SDK_ERROR = "nextgenas.sdk.editor in settings does not point to a valid SDK. Requires Apache FlexJS 0.8.0 or newer.";
@@ -45,34 +45,18 @@ const INITIALIZING_MESSAGE = "Initializing ActionScript and MXML language server
 const RESTART_BUTTON_LABEL = "Restart Now";
 const CONFIGURE_SDK_LABEL = "Configure SDK";
 const NO_SDK = "$(alert) No SDK";
-let savedChild: child_process.ChildProcess;
 let savedContext: vscode.ExtensionContext;
+let savedLanguageClient: LanguageClient;
 let bundledCompilerPath: string;
 let editorSDKHome: string;
 let javaExecutablePath: string;
 let frameworkSDKHome: string;
-let killed = false;
 let hasShownFlexJSSDKWarning = false;
 let hasShownFrameworkSDKWarning = false;
 let sdkStatusBarItem: vscode.StatusBarItem;
 let sourcePathDataProvider: ActionScriptSourcePathDataProvider = null;
 let actionScriptTaskProvider: ActionScriptTaskProvider = null;
 let debugConfigurationProvider: SWFDebugConfigurationProvider = null;
-
-function killJavaProcess()
-{
-	if(!savedChild)
-	{
-		return;
-	}
-	killed = true;
-	//we are killing the process on purpose, so we don't care
-	//about these events anymore
-	savedChild.removeListener("exit", childExitListener);
-	savedChild.removeListener("error", childErrorListener);
-	savedChild.kill();
-	savedChild = null;
-}
 
 function getValidatedEditorSDKConfiguration(javaExecutablePath: string): string
 {
@@ -96,18 +80,25 @@ function onDidChangeConfiguration(event)
 	let newFrameworkSDKHome = getFrameworkSDKPathWithFallbacks();
 	if(editorSDKHome != newEditorSDKHome ||
 		javaExecutablePath != newJavaExecutablePath)
-	{
-		//on Windows, the language server doesn't restart very gracefully,
-		//so force a restart if either of these two settings changes.
-		//we don't need to restart if nextgenas.sdk.framework changes
-		//because the language server can detect that and update.
-		vscode.window.showWarningMessage(RESTART_MESSAGE, RESTART_BUTTON_LABEL).then((action) =>
 		{
-			if(action === RESTART_BUTTON_LABEL)
+			//we're going to try to kill the language server and then restart
+			//it with the new settings
+			savedLanguageClient.stop().then(() =>
 			{
-				vscode.commands.executeCommand("workbench.action.reloadWindow");
-			}
-		});
+				startClient();
+			}, () =>
+			{
+				//something went wrong restarting the language server...
+				//this shouldn't happen, but if it does, the user can manually
+				//restart
+				vscode.window.showWarningMessage(RESTART_MESSAGE, RESTART_BUTTON_LABEL).then((action) =>
+				{
+					if(action === RESTART_BUTTON_LABEL)
+					{
+						vscode.commands.executeCommand("workbench.action.reloadWindow");
+					}
+				});
+			});
 	}
 	if(editorSDKHome != newEditorSDKHome ||
 		frameworkSDKHome != newFrameworkSDKHome)
@@ -139,6 +130,67 @@ export function activate(context: vscode.ExtensionContext)
 	editorSDKHome = getValidatedEditorSDKConfiguration(javaExecutablePath);
 	frameworkSDKHome = getFrameworkSDKPathWithFallbacks();
 	vscode.workspace.onDidChangeConfiguration(onDidChangeConfiguration);
+
+	vscode.languages.setLanguageConfiguration("nextgenas",
+	{
+		//this code is MIT licensed from Microsoft's official TypeScript
+		//extension that's built into VSCode
+		//https://github.com/Microsoft/vscode/blob/9d611d4dfd5a4a101b5201b8c9e21af97f06e7a7/extensions/typescript/src/typescriptMain.ts#L186
+		"onEnterRules":
+		[
+			{
+				beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+				afterText: /^\s*\*\/$/,
+				action:
+				{
+					//if you press enter between /** and */ on the same line,
+					//it will insert a * on the next line
+					indentAction: vscode.IndentAction.IndentOutdent,
+					appendText: " * "
+				}
+			},
+			{
+				beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+				action:
+				{
+					//if you press enter after /**, when there is no */, it
+					//will insert a * on the next line
+					indentAction: vscode.IndentAction.None,
+					appendText: " * "
+				}
+			},
+			{
+				beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
+				action:
+				{
+					//if you press enter on a line with *, it will insert
+					//another * on the next line
+					indentAction: vscode.IndentAction.None,
+					appendText: "* "
+				}
+			},
+			{
+				beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
+				action:
+				{
+					//removes the extra space if you press enter after a line
+					//that contains only */
+					indentAction: vscode.IndentAction.None,
+					removeText: 1
+				}
+			},
+			{
+				beforeText: /^(\t|(\ \ ))*\ \*[^/]*\*\/\s*$/,
+				action:
+				{
+					//removes the extra space if you press enter after a line
+					//that starts with * and also has */ at the end
+					indentAction: vscode.IndentAction.None,
+					removeText: 1
+				}
+			}
+		]
+	});
 	
 	//this command is deprecated and will be removed in the future
 	vscode.commands.registerCommand("nextgenas.createASConfigTaskRunner", () =>
@@ -184,7 +236,6 @@ export function activate(context: vscode.ExtensionContext)
 export function deactivate()
 {
 	 savedContext = null;
-	 killJavaProcess();
 }
 
 function childExitListener(code)
@@ -224,179 +275,6 @@ function showMissingFrameworkSDKError()
 		{
 			selectWorkspaceSDK();
 		}
-	});
-}
-
-class CustomErrorHandler implements ErrorHandler
-{
-	private restarts: number[];
-
-	constructor(private name: string)
-	{
-		this.restarts = [];
-	}
-
-	error(error: Error, message: Message, count): ErrorAction
-	{
-		//this is simply the default behavior
-		if(count && count <= 3)
-		{
-			return ErrorAction.Continue;
-		}
-		return ErrorAction.Shutdown;
-	}
-	closed(): CloseAction
-	{
-		if(killed)
-		{
-			//we killed the process on purpose, so we will attempt to restart manually
-			killed = false;
-			return CloseAction.DoNotRestart;
-		}
-		if(!javaExecutablePath)
-		{
-			//if we can't find java, we can't restart the process
-			vscode.window.showErrorMessage(MISSING_JAVA_ERROR);
-			return CloseAction.DoNotRestart;
-		}
-		if(hasInvalidEditorSDK())
-		{
-			vscode.window.showErrorMessage(INVALID_SDK_ERROR);
-			//if we can't find the SDK, we can't start the process
-			return CloseAction.DoNotRestart;
-		}
-		if(!frameworkSDKHome)
-		{
-			showMissingFrameworkSDKError();
-			//if we can't find an SDK, we can't start the process
-			return CloseAction.DoNotRestart;
-		}
-
-		//this is the default behavior. the code above handles a special case
-		//where we need to kill the process and restart it, but we don't want
-		//that to be detected below.
-		this.restarts.push(Date.now());
-		if (this.restarts.length < 5)
-		{
-			return CloseAction.Restart;
-		}
-		else
-		{
-			let diff = this.restarts[this.restarts.length - 1] - this.restarts[0];
-			if(diff <= 3 * 60 * 1000)
-			{
-				vscode.window.showErrorMessage(`The ${this.name} server crashed 5 times in the last 3 minutes. The server will not be restarted.`);
-				return CloseAction.DoNotRestart;
-			}
-			else
-			{
-				this.restarts.shift();
-				return CloseAction.Restart;
-			}
-		}
-	}
-}
-
-function createLanguageServer(): Promise<StreamInfo>
-{
-	return new Promise((resolve, reject) =>
-	{
-		//immediately reject if flexjs or java cannot be found
-		if(hasInvalidJava())
-		{
-			reject(INVALID_JAVA_ERROR)
-			return;
-		}
-		if(!javaExecutablePath)
-		{ 
-			reject(MISSING_JAVA_ERROR);
-			return;
-		}
-		if(hasInvalidEditorSDK())
-		{
-			reject(INVALID_SDK_ERROR);
-			return;
-		}
-		if(!frameworkSDKHome)
-		{
-			reject(MISSING_FRAMEWORK_SDK_ERROR);
-			return;
-		}
-		findPort(55282, (port) =>
-		{
-			let cpDelimiter = getJavaClassPathDelimiter();
-			let cp = path.resolve(savedContext.extensionPath, "bin", "*");
-			if(editorSDKHome)
-			{
-				//use the nextgenas.sdk.editor configuration
-				cp += cpDelimiter +
-					//the following jars come from apache flexjs
-					path.resolve(editorSDKHome, "lib", "*") +
-					cpDelimiter +
-					path.resolve(editorSDKHome, "lib", "external", "*") +
-					cpDelimiter +
-					path.resolve(editorSDKHome, "js", "lib", "*");
-			}
-			else
-			{
-				//use the bundled compiler
-				cp += cpDelimiter + path.join(savedContext.asAbsolutePath("./bundled-compiler"), "*");
-			}
-			let args =
-			[
-				"-cp",
-				cp,
-				//the language server communicates with vscode on this port
-				"-Dnextgeas.vscode.port=" + port,
-				"com.nextgenactionscript.vscode.Main",
-			];
-			if(frameworkSDKHome)
-			{
-				args.unshift("-Dflexlib=" + path.join(frameworkSDKHome, "frameworks"));
-			}
-
-			//remote java debugging
-			//args.unshift("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005");
-
-			//failed assertions in the compiler will crash the extension,
-			//so this should not be enabled by default, even for debugging
-			//args.unshift("-ea");
-			
-			let server = net.createServer(socket =>
-			{
-				resolve(
-				{
-					reader: socket,
-					writer: socket
-				});
-			});
-			server.listen(port, () =>
-			{
-				let options =
-				{
-					cwd: vscode.workspace.rootPath
-				};
-				
-				// Start the child java process
-				savedChild = child_process.spawn(javaExecutablePath, args, options);
-				savedChild.on("error", childErrorListener);
-				savedChild.on("exit", childExitListener);
-				if(savedChild.stdout)
-				{
-					savedChild.stdout.on("data", (data: Buffer) =>
-					{
-						console.log(data.toString("utf8"));
-					});
-				}
-				if(savedChild.stderr)
-				{
-					savedChild.stderr.on("data", (data: Buffer) =>
-					{
-						console.error(data.toString("utf8"));
-					});
-				}
-			});
-		});
 	});
 }
 
@@ -464,75 +342,52 @@ function startClient()
 						vscode.workspace.createFileSystemWatcher("**/*.as"),
 						vscode.workspace.createFileSystemWatcher("**/*.mxml"),
 					]
-				},
-				errorHandler: new CustomErrorHandler("ActionScript and MXML")
+				}
 			};
-			vscode.languages.setLanguageConfiguration("nextgenas",
+			let cpDelimiter = getJavaClassPathDelimiter();
+			let cp = path.resolve(savedContext.extensionPath, "bin", "*");
+			if(editorSDKHome)
 			{
-				//this code is MIT licensed from Microsoft's official TypeScript
-				//extension that's built into VSCode
-				//https://github.com/Microsoft/vscode/blob/9d611d4dfd5a4a101b5201b8c9e21af97f06e7a7/extensions/typescript/src/typescriptMain.ts#L186
-				"onEnterRules":
-				[
-					{
-						beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-						afterText: /^\s*\*\/$/,
-						action:
-						{
-							//if you press enter between /** and */ on the same line,
-							//it will insert a * on the next line
-							indentAction: vscode.IndentAction.IndentOutdent,
-							appendText: " * "
-						}
-					},
-					{
-						beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-						action:
-						{
-							//if you press enter after /**, when there is no */, it
-							//will insert a * on the next line
-							indentAction: vscode.IndentAction.None,
-							appendText: " * "
-						}
-					},
-					{
-						beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
-						action:
-						{
-							//if you press enter on a line with *, it will insert
-							//another * on the next line
-							indentAction: vscode.IndentAction.None,
-							appendText: "* "
-						}
-					},
-					{
-						beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
-						action:
-						{
-							//removes the extra space if you press enter after a line
-							//that contains only */
-							indentAction: vscode.IndentAction.None,
-							removeText: 1
-						}
-					},
-					{
-						beforeText: /^(\t|(\ \ ))*\ \*[^/]*\*\/\s*$/,
-						action:
-						{
-							//removes the extra space if you press enter after a line
-							//that starts with * and also has */ at the end
-							indentAction: vscode.IndentAction.None,
-							removeText: 1
-						}
-					}
-				]
-			});
-			let languageClient = new LanguageClient("nextgenas", "ActionScript and MXML Language Server", createLanguageServer, clientOptions);
-			languageClient.onReady().then(() =>
+				//use the nextgenas.sdk.editor configuration
+				cp += cpDelimiter +
+					//the following jars come from apache flexjs
+					path.resolve(editorSDKHome, "lib", "*") +
+					cpDelimiter +
+					path.resolve(editorSDKHome, "lib", "external", "*") +
+					cpDelimiter +
+					path.resolve(editorSDKHome, "js", "lib", "*");
+			}
+			else
+			{
+				//use the bundled compiler
+				cp += cpDelimiter + path.join(savedContext.asAbsolutePath("./bundled-compiler"), "*");
+			}
+			let args =
+			[
+				"-cp",
+				cp,
+				"com.nextgenactionscript.vscode.Main",
+			];
+			if(frameworkSDKHome)
+			{
+				args.unshift("-Dflexlib=" + path.join(frameworkSDKHome, "frameworks"));
+			}
+			let executable: Executable =
+			{
+				command: javaExecutablePath,
+				args: args,
+				options:
+				{
+					cwd: vscode.workspace.rootPath
+				}
+			};
+			let options: ExecutableOptions;
+			savedLanguageClient = new LanguageClient("nextgenas", "ActionScript and MXML Language Server", executable, clientOptions);
+			savedLanguageClient.onReady().then(() =>
 			{
 				resolve();
 			});
-			let disposable = languageClient.start();
+			let disposable = savedLanguageClient.start();
 			savedContext.subscriptions.push(disposable);
 		});
 	});
