7d6d8fde52df461392a39b2593640dc2b18867bb
Josh Tynjala <joshtynjala@gmail.com>
2018-01-25 09:13:06 -0800

    vscode-extension: added nextgenas.asconfigc.useBundled setting, just in case someone wants to use the npm version exclusively (to work around bugs in the bundled version, for example)

--- vscode-extension/src/main/ts/utils/ActionScriptTaskProvider.ts
+++ vscode-extension/src/main/ts/utils/ActionScriptTaskProvider.ts
@@ -208,7 +208,13 @@ export default class ActionScriptTaskProvider implements vscode.TaskProvider
 			{
 				return [winPath];
 			}
-			//otherwise, try to use a global executable
+			let useBundled = <string> vscode.workspace.getConfiguration("nextgenas").get("asconfigc.useBundled");
+			if(!useBundled)
+			{
+				//use an executable on the system path
+				return [executableName];
+			}
+			//use the version bundled with the extension
 			return this.getDefaultCommand();
 		}
 		let executableName = "asconfigc";
@@ -217,6 +223,13 @@ export default class ActionScriptTaskProvider implements vscode.TaskProvider
 		{
 			return [unixPath];
 		}
+		let useBundled = <string> vscode.workspace.getConfiguration("nextgenas").get("asconfigc.useBundled");
+		if(!useBundled)
+		{
+			//use an executable on the system path
+			return [executableName];
+		}
+		//use the version bundled with the extension
 		return this.getDefaultCommand();
 	}
 	
