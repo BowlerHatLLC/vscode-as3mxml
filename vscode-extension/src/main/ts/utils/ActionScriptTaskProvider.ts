bef52d97cf56ba5555b22c4d06aa5639369387f3
Josh Tynjala <joshtynjala@gmail.com>
2018-11-14 11:59:17 -0800

    vscode-extension: added new clean task for ActionScript projects

--- vscode-extension/src/main/ts/utils/ActionScriptTaskProvider.ts
+++ vscode-extension/src/main/ts/utils/ActionScriptTaskProvider.ts
@@ -42,9 +42,10 @@ const TASK_TYPE = "actionscript";
 
 interface ActionScriptTaskDefinition extends vscode.TaskDefinition
 {
-	debug: boolean;
+	debug?: boolean;
 	air?: string;
 	asconfig?: string;
+	clean?: boolean;
 }
 
 export default class ActionScriptTaskProvider implements vscode.TaskProvider
@@ -141,6 +142,8 @@ export default class ActionScriptTaskProvider implements vscode.TaskProvider
 			jsonURI, workspaceFolder, command, frameworkSDK, true, null));
 		result.push(this.getTask("compile release" + taskNameSuffix,
 			jsonURI, workspaceFolder, command, frameworkSDK, false, null));
+		result.push(this.getCleanTask("clean" + taskNameSuffix,
+			jsonURI, workspaceFolder, command, frameworkSDK));
 
 		//package mobile AIR application
 		if(isAIRMobile)
@@ -303,6 +306,43 @@ export default class ActionScriptTaskProvider implements vscode.TaskProvider
 		let execution = new vscode.ProcessExecution(command[0], options);
 		let task = new vscode.Task(definition, workspaceFolder, description,
 			source, execution, MATCHER);
+		task.group = vscode.TaskGroup.Build;
+		return task;
+	}
+
+	private getCleanTask(description: string, jsonURI: vscode.Uri,
+		workspaceFolder: vscode.WorkspaceFolder, command: string[], sdk: string): vscode.Task
+	{
+		let asconfig: string = undefined;
+		if(jsonURI)
+		{
+			let rootJSON = path.resolve(workspaceFolder.uri.fsPath, ASCONFIG_JSON);
+			if(rootJSON !== jsonURI.fsPath)
+			{
+				//the asconfig field should remain empty if it's the root
+				//asconfig.json in the workspace.
+				//this is different than TypeScript because we didn't originally
+				//create tasks for additional asconfig files in the workspace, and
+				//we don't want to break old tasks.json files that already existed
+				//before this feature was added.
+				//ideally, we'd be able to use resolveTask() to populate the
+				//asconfig field, but that function never seems to be called.
+				asconfig = jsonURI.toString().substr(workspaceFolder.uri.toString().length + 1);
+			}
+		}
+		let definition: ActionScriptTaskDefinition = { type: TASK_TYPE, asconfig, clean: true };
+		let options = ["--sdk", sdk, "--clean=true"];
+		if(jsonURI)
+		{
+			options.push("--project", jsonURI.fsPath);
+		}
+		if(command.length > 1)
+		{
+			options.unshift(...command.slice(1));
+		}
+		let execution = new vscode.ProcessExecution(command[0], options);
+		let task = new vscode.Task(definition, workspaceFolder, description,
+			"ActionScript", execution, MATCHER);
 		task.group = vscode.TaskGroup.Build;
 		return task;
 	}
