e4d52716eba741c011b6922f2cc28f318a35db2b
Josh Tynjala <joshtynjala@bowlerhat.dev>
2019-10-30 09:34:52 -0700

    swf-debugger: fixed issue where launching failed for some SDKs
    In particular, the Flex SDK does not have a certain method overload that was being used. Switched to the one that exists in all versions.

--- swf-debugger/src/main/java/com/as3mxml/vscode/SWFDebugSession.java
+++ swf-debugger/src/main/java/com/as3mxml/vscode/SWFDebugSession.java
@@ -595,13 +595,15 @@ public class SWFDebugSession extends DebugSession
                     //notice that we use the value of launcher if it isn't null, but
                     //we don't actually use the value of player. player's purpose is
                     //more about verifying that a runtime can be auto-detected.
+                    swfSession = (ThreadSafeSession) manager.launch(program, airLaunchInfo, !swfArgs.noDebug, null, null);
+                    //there's a launch() overload that accepts the launcher as
+                    //a final parameter in some versions of the debugger, but
+                    //not all of them (in particular, the Flex SDK doesn't have
+                    //this overload). For maximum compatibility, we should pass
+                    //the launcher to the session manually.
                     if(launcher != null)
                     {
-                        swfSession = (ThreadSafeSession) manager.launch(program, airLaunchInfo, !swfArgs.noDebug, null, null, launcher);
-                    }
-                    else
-                    {
-                        swfSession = (ThreadSafeSession) manager.launch(program, airLaunchInfo, !swfArgs.noDebug, null, null);
+                        swfSession.setLauncher(launcher);
                     }
                 }
             }
