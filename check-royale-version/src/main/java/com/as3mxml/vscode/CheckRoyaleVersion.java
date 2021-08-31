da44548b4fa6b08eb123488cde22279ed23e7f2c
Josh Tynjala <joshtynjala@bowlerhat.dev>
2021-08-31 09:34:11 -0700

    Apache Royale 0.9.8

--- check-royale-version/src/main/java/com/as3mxml/vscode/CheckRoyaleVersion.java
+++ check-royale-version/src/main/java/com/as3mxml/vscode/CheckRoyaleVersion.java
@@ -28,7 +28,7 @@ public class CheckRoyaleVersion {
 
     private static final int MIN_MAJOR = 0;
     private static final int MIN_MINOR = 9;
-    private static final int MIN_REVISION = 7;
+    private static final int MIN_REVISION = 8;
 
     public static void main(String[] args) {
         try {
