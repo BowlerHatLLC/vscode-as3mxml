ef09f536ee671b0496210b8401b3c914d939dbf7
Josh Tynjala <joshtynjala@bowlerhat.dev>
2022-07-11 11:07:52 -0700

    vscode-extension: don't allow multiple quick compile sessions to be active at the same time

--- vscode-extension/src/main/ts/commands/quickCompileAndLaunch.ts
+++ vscode-extension/src/main/ts/commands/quickCompileAndLaunch.ts
@@ -23,6 +23,8 @@ import findAnimate from "../utils/findAnimate";
 const QUICK_COMPILE_MESSAGE = "Building ActionScript & MXML project...";
 const CANNOT_LAUNCH_QUICK_COMPILE_FAILED_ERROR =
   "Quick compile failed with errors. Launch cancelled.";
+const PREVIOUS_QUICK_COMPILE_NOT_COMPLETE_MESSAGE =
+  "Quick compile not started. A previous quick compile has not completed yet.";
 
 export default function quickCompileAndLaunch(uris: string[], debug: boolean) {
   if (uris.length === 0) {
@@ -47,91 +49,102 @@ export default function quickCompileAndLaunch(uris: string[], debug: boolean) {
   }
 }
 
+let quickCompileActive = false;
+
 async function quickCompileAndLaunchURI(uri: string, debug: boolean) {
   if (!uri) {
     //it's possible for no folder to be chosen when using
     //showWorkspaceFolderPick()
     return;
   }
-  //before running a task, VSCode saves all files. we should do the same
-  //before running a quick compile, since it's like a task.
-  await vscode.commands.executeCommand("workbench.action.files.saveAll");
-  vscode.window.withProgress(
-    { location: vscode.ProgressLocation.Window },
-    (progress) => {
-      progress.report({ message: QUICK_COMPILE_MESSAGE });
-      return new Promise<void>((resolve, reject) => {
-        return vscode.commands
-          .executeCommand("workbench.action.debug.stop")
-          .then(() => {
-            let animateFile = getAnimateFile(uri);
-            if (animateFile) {
-              let animatePath = findAnimate();
+  if (quickCompileActive) {
+    vscode.window.showErrorMessage(PREVIOUS_QUICK_COMPILE_NOT_COMPLETE_MESSAGE);
+    return;
+  }
+  quickCompileActive = true;
+  try {
+    //before running a task, VSCode saves all files. we should do the same
+    //before running a quick compile, since it's like a task.
+    await vscode.commands.executeCommand("workbench.action.files.saveAll");
+    await vscode.window.withProgress(
+      { location: vscode.ProgressLocation.Window },
+      (progress) => {
+        progress.report({ message: QUICK_COMPILE_MESSAGE });
+        return new Promise<void>((resolve, reject) => {
+          return vscode.commands
+            .executeCommand("workbench.action.debug.stop")
+            .then(() => {
+              let animateFile = getAnimateFile(uri);
+              if (animateFile) {
+                let animatePath = findAnimate();
 
-              let extension = vscode.extensions.getExtension(
-                "bowlerhatllc.vscode-as3mxml"
-              );
-              let fileName = debug ? "debug-movie.jsfl" : "test-movie.jsfl";
-              let jsflPath = path.resolve(
-                extension.extensionPath,
-                "jsfl",
-                fileName
-              );
+                let extension = vscode.extensions.getExtension(
+                  "bowlerhatllc.vscode-as3mxml"
+                );
+                let fileName = debug ? "debug-movie.jsfl" : "test-movie.jsfl";
+                let jsflPath = path.resolve(
+                  extension.extensionPath,
+                  "jsfl",
+                  fileName
+                );
 
-              if (process.platform === "win32") {
-                child_process.spawn(animatePath, [animateFile, jsflPath]);
-              } else if (process.platform === "darwin") {
-                //macOS
-                child_process.spawn("open", [
-                  "-a",
-                  animatePath,
-                  animateFile,
-                  jsflPath,
-                ]);
-              } else {
-                reject();
+                if (process.platform === "win32") {
+                  child_process.spawn(animatePath, [animateFile, jsflPath]);
+                } else if (process.platform === "darwin") {
+                  //macOS
+                  child_process.spawn("open", [
+                    "-a",
+                    animatePath,
+                    animateFile,
+                    jsflPath,
+                  ]);
+                } else {
+                  reject();
+                  return;
+                }
+
+                resolve();
                 return;
               }
+              return vscode.commands
+                .executeCommand("as3mxml.quickCompile", uri, debug)
+                .then(
+                  (result) => {
+                    resolve();
 
-              resolve();
-              return;
-            }
-            return vscode.commands
-              .executeCommand("as3mxml.quickCompile", uri, debug)
-              .then(
-                (result) => {
-                  resolve();
-
-                  if (result === true) {
-                    if (debug) {
-                      vscode.commands.executeCommand(
-                        "workbench.action.debug.start"
-                      );
+                    if (result === true) {
+                      if (debug) {
+                        vscode.commands.executeCommand(
+                          "workbench.action.debug.start"
+                        );
+                      } else {
+                        vscode.commands.executeCommand(
+                          "workbench.action.debug.run"
+                        );
+                      }
                     } else {
-                      vscode.commands.executeCommand(
-                        "workbench.action.debug.run"
+                      vscode.window.showErrorMessage(
+                        CANNOT_LAUNCH_QUICK_COMPILE_FAILED_ERROR
                       );
                     }
-                  } else {
+                  },
+                  () => {
+                    resolve();
+
+                    //if the build failed, notify the user that we're not starting
+                    //a debug session
                     vscode.window.showErrorMessage(
                       CANNOT_LAUNCH_QUICK_COMPILE_FAILED_ERROR
                     );
                   }
-                },
-                () => {
-                  resolve();
-
-                  //if the build failed, notify the user that we're not starting
-                  //a debug session
-                  vscode.window.showErrorMessage(
-                    CANNOT_LAUNCH_QUICK_COMPILE_FAILED_ERROR
-                  );
-                }
-              );
-          });
-      });
-    }
-  );
+                );
+            });
+        });
+      }
+    );
+  } finally {
+    quickCompileActive = false;
+  }
 }
 
 function getAnimateFile(uri: string) {
