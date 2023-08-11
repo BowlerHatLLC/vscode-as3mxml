b493489181d817ce39b5280b59544e31a9ef3f0d
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-08-11 15:33:21 -0700

    UnusedImportProblem: remove workaround for bug in older Royale compiler

--- language-server/src/main/java/com/as3mxml/vscode/compiler/problems/UnusedImportProblem.java
+++ language-server/src/main/java/com/as3mxml/vscode/compiler/problems/UnusedImportProblem.java
@@ -15,13 +15,9 @@ limitations under the License.
 */
 package com.as3mxml.vscode.compiler.problems;
 
-import org.apache.royale.compiler.common.ISourceLocation;
-import org.apache.royale.compiler.common.SourceLocation;
-import org.apache.royale.compiler.internal.tree.as.FullNameNode;
 import org.apache.royale.compiler.problems.CompilerProblem;
 import org.apache.royale.compiler.problems.CompilerProblemSeverity;
 import org.apache.royale.compiler.problems.annotations.DefaultSeverity;
-import org.apache.royale.compiler.tree.as.IExpressionNode;
 import org.apache.royale.compiler.tree.as.IImportNode;
 
 @DefaultSeverity(CompilerProblemSeverity.WARNING)
@@ -30,24 +26,9 @@ public class UnusedImportProblem extends CompilerProblem {
 	public static final String DIAGNOSTIC_CODE = "as3mxml-unused-import";
 
 	public UnusedImportProblem(IImportNode site) {
-		// TODO: change this to the full import node when royale-compiler range gets
-		// fixed
-		// this is a hack because royale-compiler does not return the correct
-		// range for import nodes.
-		super(getSite(site));
+		super(site);
 		importName = site.getImportName();
 	}
 
 	public String importName;
-
-	private static ISourceLocation getSite(IImportNode importNode) {
-		SourceLocation location = new SourceLocation();
-		IExpressionNode importNameNode = importNode.getImportNameNode();
-		if (importNameNode instanceof FullNameNode) {
-			FullNameNode fullName = (FullNameNode) importNameNode;
-			importNameNode = fullName.getRightOperandNode();
-		}
-		location.span(importNode, importNameNode);
-		return location;
-	}
 }
